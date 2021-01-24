ce0cf96204a56d600c5f9c32f5cc681bc0dfcd36
Marwa Sh <marwa.al.shahal@gmail.com>
2021-01-24 13:49:34 +0000

    Automatically backed up by Learn

--- src/components/MovieShow.js
+++ src/components/MovieShow.js
@@ -1,10 +1,13 @@
 import React from 'react';
  
-const MovieShow = props => {
- 
+// Here we add `match` to the arguments so we can access the path information 
+// in `routerProps` that is passed from MoviesPage.js 
+const MovieShow = ({match, movies}) => {
   return (
     <div>
-      <h3>Movies Show Component!</h3>
+      // And here we access the `movieId` stored in match.params to render 
+      // information about the selected movie
+      <h3>{ movies[match.params.movieId].title }</h3>
     </div>
   );
 }
